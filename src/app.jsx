37a33b49a0a36aca50e3f223012794433880df37
Bon <bng22061996@gmail.com>
2018-04-04 11:11:35 +0700

    update

--- src/app.jsx
+++
@@ -1,6 +0,0 @@
-const React = require('react');
-const ReactDOM = require('react-dom');
-
-ReactDOM.render(
-    React.createElement('h1', null, "Hello React"), document.getElementById('root')
-)
--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,30 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/app.jsx',
+  entry: './src/app.js',
   output: {
     path: path.resolve(__dirname, 'public'),
     filename: 'bundle.js'
+  },
+  resolve: {
+    modules: [__dirname, 'node_modules'],
+    alias: {
+      reducer: 'src/reducers/reducer.js',
+      store  : 'src/example.js',
+      List   : 'src/components/List.js',
+      Note   : 'src/components/Note.js',
+      NoteForm : 'src/components/NoteForm.js',
+      action : 'src/action.js'
+    }
   },
   module: {
     rules: [
       {
         loader: 'babel-loader',
         query: {
-          presets: ['react','es2015']
+          presets: ['react','es2015','stage-0']
         },
-        test: /\.jsx?$/,
+        test: /\.js?$/,
         exclude: /node_modules/
       }
     ]
